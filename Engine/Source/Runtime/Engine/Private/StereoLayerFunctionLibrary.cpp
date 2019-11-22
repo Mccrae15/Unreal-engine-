4c687bd4dc9dfe463e9713d96b9c98c49d7012c9
Daniel <truejelly800@gmail.com>
2019-11-22 11:36:38 +0100

    * TextBlock - wrapowanie textu - wyexposowano zmienną * StereoLayer - sprawdzanie widoczności splasha

--- Engine/Source/Runtime/Engine/Private/StereoLayerFunctionLibrary.cpp
+++ Engine/Source/Runtime/Engine/Private/StereoLayerFunctionLibrary.cpp
@@ -106,6 +106,16 @@ void UStereoLayerFunctionLibrary::ShowSplashScreen()
 	}
 }
 
+bool UStereoLayerFunctionLibrary::IsSplashScreenVisible()
+{
+	IStereoLayers* StereoLayers = GetStereoLayers();
+	if (StereoLayers)
+	{
+		return StereoLayers->IsSplashscreenVisible();
+	}
+	return false;
+}
+
 void UStereoLayerFunctionLibrary::HideSplashScreen()
 {
 	IXRLoadingScreen* LoadingScreen = GetLoadingScreen();
