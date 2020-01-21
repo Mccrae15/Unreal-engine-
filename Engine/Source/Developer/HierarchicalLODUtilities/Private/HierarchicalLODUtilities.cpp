13d04b08a660afd7ae60b8db145cc08966213d97
Daniel <truejelly800@gmail.com>
2020-01-21 14:34:31 +0100

    * Wyłączenie wykluczenia HLOD Actorów z  manualnego generowania HLODÓW

--- Engine/Source/Developer/HierarchicalLODUtilities/Private/HierarchicalLODUtilities.cpp
+++ Engine/Source/Developer/HierarchicalLODUtilities/Private/HierarchicalLODUtilities.cpp
@@ -592,7 +592,7 @@ EClusterGenerationError FHierarchicalLODUtilities::ShouldGenerateCluster(AActor*
 	ALODActor* LODActor = Cast<ALODActor>(Actor);
 	if (LODActor)
 	{
-		return EClusterGenerationError::LODActor;
+		return EClusterGenerationError::/*LODActor*/ValidActor;
 	}
 
 	FVector Origin, Extent;
