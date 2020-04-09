997927c517e549cf7ba90ef53027077cad9cfa98
Stu McKenna <Stu.McKenna@epicgames.com>
2020-04-09 18:15:31 -0400

    - Fix crash in GetFilteredSocketTransform when you have no elements in the array - Fix incorrect transform application (and reading from component) in GetFilteredSocketTransform - Remove stub GPU implementation of GetFilteredSocketTransform so an stack error occurs, the new functions shoud be used instead - Fix for not applying the rotation on the GPU for gathering bone transforms
    #rb jamie.hayes
    #jira UE-91746
    #lockdown marcus.wassmer
    
    [CL 12706400 by Stu McKenna in 4.25 branch]

--- Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraDataInterfaceSkeletalMesh.h
+++ Engine/Plugins/FX/Niagara/Source/Niagara/Classes/NiagaraDataInterfaceSkeletalMesh.h
@@ -714,6 +714,8 @@ public:
 	static const FString FilteredSocketBoneOffsetName;
 	static const FString InstanceTransformName;
 	static const FString InstancePrevTransformName;
+	static const FString InstanceRotationName;
+	static const FString InstancePrevRotationName;
 	static const FString InstanceInvDeltaTimeName;
 	static const FString EnabledFeaturesName;
 
