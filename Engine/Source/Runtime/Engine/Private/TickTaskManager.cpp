e8a42fb58936830380c4856cc6edb7954b5b640e
Arciel Rekman <arciel.rekman@epicgames.com>
2020-04-23 13:13:47 -0400

    Fix TSan (UE-91609).
    #rb none
    #fyi Brandon.Schaefer
    #jira UE-91609
    
    [CL 13009018 by Arciel Rekman in 4.25 branch]

--- Engine/Source/Runtime/Engine/Private/TickTaskManager.cpp
+++ Engine/Source/Runtime/Engine/Private/TickTaskManager.cpp
@@ -1989,7 +1989,7 @@ void FTickFunction::QueueTickFunctionParallel(const struct FTickContext& TickCon
 				{
 					FTickFunction* Prereq = Prerequisites[PrereqIndex].Get();
 #if USING_THREAD_SANITISER
-					if (Prereq) { TSAN_AFTER(&Prereq->TickQueuedGFrameCounter); }
+					if (Prereq) { TSAN_AFTER(&InternalData->TickQueuedGFrameCounter); }
 #endif
 					if (!Prereq)
 					{
