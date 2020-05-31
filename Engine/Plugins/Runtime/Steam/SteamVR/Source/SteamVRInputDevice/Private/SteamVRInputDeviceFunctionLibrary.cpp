13144420573dc0eb8c84489e929e1a44608be7a8
Daniel <truejelly800@gmail.com>
2020-05-31 13:58:10 +0200

    USteamVRInputDeviceFunctionLibrary::GetSteamVRInputDevice() powoduje crashe, kiedy FSteamVRInputDevice.DeviceSignature nie jest zapopulowane -fix

--- Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVRInputDevice/Private/SteamVRInputDeviceFunctionLibrary.cpp
+++ Engine/Plugins/Runtime/Steam/SteamVR/Source/SteamVRInputDevice/Private/SteamVRInputDeviceFunctionLibrary.cpp
@@ -843,15 +843,21 @@ FSteamVRInputDevice* USteamVRInputDeviceFunctionLibrary::GetSteamVRInputDevice()
 	for (auto MotionController : MotionControllers)
 	{
 		FSteamVRInputDevice* TestSteamVRDevice = static_cast<FSteamVRInputDevice*>(MotionController);
-		if (TestSteamVRDevice != nullptr && !FGenericPlatformMath::IsNaN(TestSteamVRDevice->DeviceSignature) && TestSteamVRDevice->DeviceSignature == 2019)
+		if (TestSteamVRDevice != nullptr)
 		{
-			if (TestSteamVRDevice->SteamVRHMDModule && TestSteamVRDevice->SteamVRHMDModule->GetVRSystem())
+			if (!FGenericPlatformMath::IsNaN(TestSteamVRDevice->GetDeviceSignature()))
 			{
-				return TestSteamVRDevice;
-			}
-			else
-			{
-				return nullptr;
+				if (TestSteamVRDevice->GetDeviceSignature() == 2019)
+				{
+					if (TestSteamVRDevice->SteamVRHMDModule && TestSteamVRDevice->SteamVRHMDModule->GetVRSystem())
+					{
+						return TestSteamVRDevice;
+					}
+					else
+					{
+						return nullptr;
+					}
+				}
 			}
 		}
 	}
