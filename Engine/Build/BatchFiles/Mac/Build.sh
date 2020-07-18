a195c1456185a003a5a7680184aa69cd13eb7592
ben marsh <Ben.Marsh@epicgames.com>
2020-07-18 09:10:34 -0400

    Backout 13855934 - no longer rebuild UBT prior to compilation on Mac. Causes issues with remote compilation. Will redo in main.
    #jira UE-95549
    #rb na
    #lockdown cristina.riveron
    [FYI] ben.marsh
    
    #ROBOMERGE-SOURCE: CL 13890025 in //UE4/Release-4.25/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25 -> Release-4.25Plus) (v717-13788717)
    
    [CL 13890026 by ben marsh in 4.25-Plus branch]

--- Engine/Build/BatchFiles/Mac/Build.sh
+++ Engine/Build/BatchFiles/Mac/Build.sh
@@ -4,11 +4,6 @@ cd "`dirname "$0"`/../../../.."
 
 # Setup Mono
 source Engine/Build/BatchFiles/Mac/SetupMono.sh Engine/Build/BatchFiles/Mac
-# First make sure that the UnrealBuildTool is up-to-date
-if ! xbuild /property:Configuration=Development /verbosity:quiet /nologo /p:NoWarn=1591 Engine/Source/Programs/UnrealBuildTool/UnrealBuildTool.csproj; then
-  echo "Failed to build to build tool (UnrealBuildTool)"
-  exit 1
-fi
 
 if [ "$4" == "-buildscw" ] || [ "$5" == "-buildscw" ]; then
 	echo Building ShaderCompileWorker...
