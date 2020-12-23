f20dbcb9db0160bab88024ba0400e174b0a479fa
Daniel Kisielewski <truejelly800@gmail.com>
2020-12-23 08:32:22 +0100

    * zmiany konieczne do wykorzystywania PSO cache
    # Conflicts:
    #	Engine/Source/Runtime/RenderCore/Private/ShaderPipelineCache.cpp

--- Engine/Source/Runtime/OpenGLDrv/Private/OpenGLDevice.cpp
+++ Engine/Source/Runtime/OpenGLDrv/Private/OpenGLDevice.cpp
@@ -1287,6 +1287,7 @@ void FOpenGLDynamicRHI::Init()
 	GRHICommandList.GetImmediateAsyncComputeCommandList().SetComputeContext(RHIGetDefaultAsyncComputeContext());
 	FRenderResource::InitPreRHIResources();
 	GIsRHIInitialized = true;
+	RegisterSharedShaderCodeDelegates();
 }
 
 void FOpenGLDynamicRHI::PostInit()
