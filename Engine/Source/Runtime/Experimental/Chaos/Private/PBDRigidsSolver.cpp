209cd64cfe181cf972712544acff9f55d46cefde
Chris Caulfield <Chris.Caulfield@epicgames.com>
2021-01-29 11:44:29 -0500

    Chaos - DebugDraw optimizations and draw limits
    #jira none
    #rb ori.cohen
    
    [CL 15261654 by Chris Caulfield in 4.26-Chaos branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/PBDRigidsSolver.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/PBDRigidsSolver.cpp
@@ -1248,6 +1248,7 @@ namespace Chaos
 	void TPBDRigidsSolver<Traits>::PostTickDebugDraw(FReal Dt) const
 	{
 #if CHAOS_DEBUG_DRAW
+		QUICK_SCOPE_CYCLE_COUNTER(SolverDebugDraw);
 		if (ChaosSolverDebugDrawShapes == 1)
 		{
 			DebugDraw::DrawParticleShapes(FRigidTransform3(), Particles.GetActiveStaticParticlesView(), FColor(128, 0, 0), &ChaosSolverDebugDebugDrawSettings);
