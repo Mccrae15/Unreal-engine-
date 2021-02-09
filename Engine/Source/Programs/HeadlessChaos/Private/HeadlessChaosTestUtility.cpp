f4734df7cef73dcbf144a42e06dbe532ee840f1e
jaco vandyk <Jaco.VanDyk@epicgames.com>
2021-02-09 09:57:18 -0500

    HeadlessChaos fixes due to changes in FConvex
    #rb trivial
    #fyi Michael.Lentine, Benn.Gallagher, Chris.Caulfield
    #jira none
    
    [CL 15365689 by jaco vandyk in 4.26-Chaos branch]

--- Engine/Source/Programs/HeadlessChaos/Private/HeadlessChaosTestUtility.cpp
+++ Engine/Source/Programs/HeadlessChaos/Private/HeadlessChaosTestUtility.cpp
@@ -772,9 +772,7 @@ namespace ChaosTest {
 			FVec3(HalfSize.X, -HalfSize.Y,  HalfSize.Z),
 		};
 
-		TParticles<FReal, 3> BoxParticles(MoveTemp(BoxVerts));
-
-		return FImplicitConvex3(BoxParticles, Margin);
+		return FImplicitConvex3(BoxVerts, Margin);
 	}
 
 
