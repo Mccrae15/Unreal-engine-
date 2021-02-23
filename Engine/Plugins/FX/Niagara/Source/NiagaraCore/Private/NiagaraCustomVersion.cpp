8998ded7ce9e54c33592109a98042311ee1cceb3
rob krajcarski <Rob.Krajcarski@epicgames.com>
2021-02-23 14:23:59 -0400

    Fix FNiagaraDataInterfaceArrayBool to use a Buffer<uint> on the GPU #jira UE-108753, UE-108089 #rb stu.mckenna
    #ROBOMERGE-OWNER: rob.krajcarski
    #ROBOMERGE-AUTHOR: rob.krajcarski
    #ROBOMERGE-SOURCE: CL 15502727 in //UE5/Main/...
    #ROBOMERGE-BOT: STARSHIP (Main -> Release-5.0-EarlyAccess) (v771-15082668)
    #ROBOMERGE-CONFLICT from-shelf
    
    [CL 15502902 by rob krajcarski in ue5-early-access branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,5 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0xFB44A69B, 0x7F104E2F, 0xB0759325, 0xE1F23C41);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x5F4D3C59, 0xFED04632, 0xBC001118, 0x408CDC85);
