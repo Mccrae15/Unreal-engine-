09d4118bb80a41de502816e0ddb5ed94fd9871f8
ryan durand <Ryan.Durand@epicgames.com>
2021-02-24 16:24:04 -0500

    Remove deprecated timestamp server and add more resilience to signing.
    #rnx
    #rb none
    #jira UE-109293
    
    #ROBOMERGE-SOURCE: CL 15521604 in //UE4/Release-4.25Plus/...
    #ROBOMERGE-BOT: RELEASE (Release-4.25Plus -> Release-4.26-Chaos) (v771-15082668)
    
    [CL 15521676 by ryan durand in 4.26-Chaos branch]

--- Engine/Source/Programs/AutomationTool/AutomationUtils/CommandUtils.cs
+++ Engine/Source/Programs/AutomationTool/AutomationUtils/CommandUtils.cs
@@ -2711,7 +2711,8 @@ namespace AutomationTool
 		static readonly string[] TimestampServersSHA1 =
 		{
 			"http://timestamp.globalsign.com/scripts/timstamp.dll",
-			"http://timestamp.comodoca.com/authenticode"
+			"http://timestamp.comodoca.com/authenticode",
+			"http://timestamp.digicert.com"
 		};
 
 		static readonly string[] TimestampServersSHA256 =
@@ -2778,7 +2779,7 @@ namespace AutomationTool
 						break;
 					}
 
-					if (Timer.Elapsed.TotalMinutes > 3.0)
+					if (Timer.Elapsed.TotalMinutes > 3.0 && NumAttempts >= 6)
 					{
 						throw new AutomationException("Failed to sign files {0} times over a period of {1}", NumAttempts, Timer.Elapsed);
 					}
