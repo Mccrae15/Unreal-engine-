b357ba23ef1c983720234e13d70222c713c0dafc
Ola Olsson <Ola.Olsson@epicgames.com>
2021-02-25 05:03:09 -0400

    Added (read only) cvar r.Shadow.Virtual.NonNaniteVSM to toggle non-nanite VSM support
    #rb Brian.Karis,Andrew.Lauritzen
    
    [CL 15526905 by Ola Olsson in ue5-early-access branch]

--- Engine/Source/Runtime/Renderer/Private/SceneRendering.h
+++ Engine/Source/Runtime/Renderer/Private/SceneRendering.h
@@ -1687,9 +1687,7 @@ struct FSortedShadowMaps
 	TArray<FProjectedShadowInfo*, SceneRenderingAllocator> VirtualShadowMapShadows;
 
 	TArray<TSharedPtr<FVirtualShadowMapClipmap>, SceneRenderingAllocator> VirtualShadowMapClipmaps;
-#if ENABLE_NON_NANITE_VSM
 	TArray<FProjectedShadowInfo*, SceneRenderingAllocator> VirtualShadowClipmapsHw;
-#endif // ENABLE_NON_NANITE_VSM
 
 	TArray<FSortedShadowMapAtlas,SceneRenderingAllocator> CompleteShadowMapAtlases;
 
