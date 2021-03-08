dda49cd57298726b7d210b543cb9bba72bd191b3
Helge Mathee <Helge.Mathee@epicgames.com>
2021-03-08 11:32:03 -0400

    Control Rig: Disallow pasting function references into other graphs
    #rb halfdan.ingvarsson
    #jira UE-109923
    
    [CL 15640011 by Helge Mathee in ue5-early-access branch]

--- Engine/Plugins/Experimental/ControlRig/Source/ControlRigDeveloper/Private/ControlRigBlueprint.cpp
+++ Engine/Plugins/Experimental/ControlRig/Source/ControlRigDeveloper/Private/ControlRigBlueprint.cpp
@@ -65,6 +65,7 @@ UControlRigBlueprint::UControlRigBlueprint(const FObjectInitializer& ObjectIniti
 	FunctionLibraryEdGraph->Initialize(this);
 
 	Model->SetDefaultFunctionLibrary(FunctionLibrary);
+	FunctionLibrary->SetDefaultFunctionLibrary(FunctionLibrary);
 
 	Validator = ObjectInitializer.CreateDefaultSubobject<UControlRigValidator>(this, TEXT("ControlRigValidator"));
 
