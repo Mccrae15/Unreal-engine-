c947893c66f362855e2f34e876cd3fe1c146696a
Michael Forot <Michael.Forot@epicgames.com>
2021-03-08 09:46:17 -0400

    Use groom gpu offset buffer for hair sim
    #rb none
    #fyi charles.derousiers
    #jira MH-3211
    
    [CL 15638185 by Michael Forot in ue5-early-access branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x5F4D3C59, 0xFED04632, 0xBC001118, 0x408CDC85);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x94C30C5C, 0x498548EB, 0x84503C66, 0x0AB7B8FE);
