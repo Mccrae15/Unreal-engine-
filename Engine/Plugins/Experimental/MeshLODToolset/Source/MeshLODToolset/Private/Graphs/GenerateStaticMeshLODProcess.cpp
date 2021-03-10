0c09996c2d360617dff365fb38f6626957f73bce
tyson brochu <tyson.brochu@epicgames.com>
2021-03-10 13:17:26 -0400

    Fix a crash when applying AutoLOD twice
    #jira UE-109767
    #rnx
    
    [CL 15667575 by tyson brochu in ue5-early-access branch]

--- Engine/Plugins/Experimental/MeshLODToolset/Source/MeshLODToolset/Private/Graphs/GenerateStaticMeshLODProcess.cpp
+++ Engine/Plugins/Experimental/MeshLODToolset/Source/MeshLODToolset/Private/Graphs/GenerateStaticMeshLODProcess.cpp
@@ -1172,7 +1172,12 @@ void UGenerateStaticMeshLODProcess::UpdateSourceStaticMeshAsset(bool bSetNewHDSo
 	TPolygonGroupAttributesRef<FName> PolygonGroupImportedMaterialSlotNames = Attributes.GetPolygonGroupMaterialSlotNames();
 	for (int32 SlotIdx : DerivedMaterialSlotIndices)
 	{
-		PolygonGroupImportedMaterialSlotNames.Set(SlotIdx, NewMaterialSet[SlotIdx].ImportedMaterialSlotName);
+		// It's possible that NewMaterialSet.Num() > PolygonGroupImportedMaterialSlotNames.GetNumElements()
+		// if there are new materials that aren't referenced by any triangles...
+		if (SlotIdx < PolygonGroupImportedMaterialSlotNames.GetNumElements())		
+		{
+			PolygonGroupImportedMaterialSlotNames.Set(SlotIdx, NewMaterialSet[SlotIdx].ImportedMaterialSlotName);
+		}
 	}
 
 	// Disable auto-generated normals/tangents, we need to use the ones we computed in LOD Generator
