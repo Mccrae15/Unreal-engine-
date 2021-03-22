78ce2c976e2b686f48c5d0af71616ba44dbdfd84
Michael Forot <Michael.Forot@epicgames.com>
2021-03-22 06:18:22 -0400

    Add init velocvity for the GC apply physics field
    #rb none
    #jira UE-110918, E-110915
    
    [CL 15758115 by Michael Forot in ue5-early-access branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/PhysicsProxy/GeometryCollectionPhysicsProxy.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/PhysicsProxy/GeometryCollectionPhysicsProxy.h
@@ -257,8 +257,6 @@ protected:
 		const TArray<int32>& ChildTransformGroupIndices,
 		const Chaos::FClusterCreationParameters<float> & Parameters);
 
-	void PushKinematicStateToSolver();
-
 	/** 
 	 * Traverses the parents of \p TransformIndex in \p GeometryCollection, counting
 	 * the number of levels until the next parent is \c INDEX_NONE.
