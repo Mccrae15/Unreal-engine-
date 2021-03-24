5a3ee4cf1cbd8732d23427487997003eb5957bd8
Daniel Wright <Daniel.Wright@epicgames.com>
2021-03-24 02:21:44 -0400

    Lumen Surface Cache captures unlit materials, useful for TV screens
    [CL 15793020 by Daniel Wright in ue5-early-access branch]

--- Engine/Source/Runtime/Renderer/Private/Lumen/LumenSceneRendering.cpp
+++ Engine/Source/Runtime/Renderer/Private/Lumen/LumenSceneRendering.cpp
@@ -367,7 +367,6 @@ void FLumenCardMeshProcessor::AddMeshBatch(const FMeshBatch& RESTRICT MeshBatch,
 		const ERasterizerCullMode MeshCullMode = ComputeMeshCullMode(MeshBatch, Material, OverrideSettings);
 
 		if (!bIsTranslucent
-			&& ShadingModels.IsLit()
 			&& (PrimitiveSceneProxy && PrimitiveSceneProxy->ShouldRenderInMainPass() && PrimitiveSceneProxy->AffectsDynamicIndirectLighting())
 			&& ShouldIncludeDomainInMeshPass(Material.GetMaterialDomain()))
 		{
