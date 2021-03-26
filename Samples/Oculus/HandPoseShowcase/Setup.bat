28e0dbb71f0c263f7259e268b6bf0c34d719c571
Loren McQuade <mcquade@fb.com>
2021-03-26 16:58:19 -0700

    Update HandPoseShowcase download link

--- Samples/Oculus/HandPoseShowcase/Setup.bat
+++ Samples/Oculus/HandPoseShowcase/Setup.bat
@@ -2,7 +2,7 @@
 setlocal EnableExtensions EnableDelayedExpansion
 cd %~dp0
 
-set source="https://scontent.oculuscdn.com/v/t64.5771-25/10000000_262987555171346_6729143540330336339_n.tar/sho_20210118_121146.tar?_nc_cat=102&ccb=2&_nc_sid=f4d450&_nc_ohc=VbxWwVpqySoAX9zXgIU&_nc_ht=scontent.oculuscdn.com&_nc_rmd=260&oh=5d7041ac54543450effd7aedae74e96e&oe=602D70CE"
+set source="https://scontent.oculuscdn.com/v/t64.5771-25/10000000_262987555171346_6729143540330336339_n.tar/sho_20210118_121146.tar?_nc_cat=102&_nc_map=control&ccb=1-3&_nc_sid=f4d450&_nc_ohc=OVQnxiuT1fsAX8w-SZw&_nc_ht=scontent.oculuscdn.com&_nc_rmd=260&oh=35591e811daeee48f2efd4bd3d5f52c9&oe=608473CE"
 set archive=%TMP%\archive_%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.tar
 
 where curl.exe > nul 2> nul && where tar.exe > nul 2> nul
--- Samples/Oculus/HandPoseShowcase/Setup.sh
+++ Samples/Oculus/HandPoseShowcase/Setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-SOURCE="https://scontent.oculuscdn.com/v/t64.5771-25/10000000_262987555171346_6729143540330336339_n.tar/sho_20210118_121146.tar?_nc_cat=102&ccb=2&_nc_sid=f4d450&_nc_ohc=VbxWwVpqySoAX9zXgIU&_nc_ht=scontent.oculuscdn.com&_nc_rmd=260&oh=5d7041ac54543450effd7aedae74e96e&oe=602D70CE"
+SOURCE="https://scontent.oculuscdn.com/v/t64.5771-25/10000000_262987555171346_6729143540330336339_n.tar/sho_20210118_121146.tar?_nc_cat=102&_nc_map=control&ccb=1-3&_nc_sid=f4d450&_nc_ohc=OVQnxiuT1fsAX8w-SZw&_nc_ht=scontent.oculuscdn.com&_nc_rmd=260&oh=35591e811daeee48f2efd4bd3d5f52c9&oe=608473CE"
 ARCHIVE=/tmp/sho_`date +%Y%m%d_%H%M%S`.tar
 
 echo Downloading Content...
