ccb6bde6c7127147ca61024ee093b1ee25d0a4c2
ben hoffman <ben.hoffman@epicgames.com>
2021-03-28 22:17:06 -0400

    Add the blueprint debugger to the "Debug" tab in blueprints
    #jira UE-110605
    #rb justin.hare
    #preflight 60611c6072faf2000111dcf2
    #preflight 606124e6cfb17300016bacf6
    #preflight 60612ff24862cc0001c8be24
    
    [CL 15848107 by ben hoffman in ue5-early-access branch]

--- Engine/Source/Editor/Kismet/Private/BlueprintEditor.cpp
+++ Engine/Source/Editor/Kismet/Private/BlueprintEditor.cpp
@@ -2756,6 +2756,11 @@ void FBlueprintEditor::CreateDefaultCommands()
 		FCanExecuteAction::CreateSP(this, &FBlueprintEditor::HasAnyWatches)
 		);
 
+	ToolkitCommands->MapAction( FBlueprintEditorCommands::Get().OpenBlueprintDebugger,
+	    FExecuteAction::CreateSP(this, &FBlueprintEditor::OpenBlueprintDebugger),
+	    FCanExecuteAction::CreateSP(this, &FBlueprintEditor::CanOpenBlueprintDebugger)
+    );
+
 	// New document actions
 	ToolkitCommands->MapAction( FBlueprintEditorCommands::Get().AddNewVariable,
 		FExecuteAction::CreateSP(this, &FBlueprintEditor::OnAddNewVariable),
@@ -3702,6 +3707,17 @@ void FBlueprintEditor::EnableAllBreakpoints()
 void FBlueprintEditor::ClearAllWatches()
 {
 	FDebuggingActionCallbacks::ClearWatches(GetBlueprintObj());
+}
+
+void FBlueprintEditor::OpenBlueprintDebugger()
+{
+	FGlobalTabmanager::Get()->TryInvokeTab(FBlueprintEditorTabs::BlueprintDebuggerID);
+}
+
+bool FBlueprintEditor::CanOpenBlueprintDebugger() const
+{
+	// The BP debugger can always be spawned because it will get updated on PIE
+	return true;
 }
 
 bool FBlueprintEditor::HasAnyBreakpoints() const
