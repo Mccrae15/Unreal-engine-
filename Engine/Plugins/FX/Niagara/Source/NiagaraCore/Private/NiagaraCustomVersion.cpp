949e3494ff671ee395d2dbbca06cf8fecae8444f
Stu McKenna <Stu.McKenna@epicgames.com>
2021-04-14 13:15:51 -0400

    - Remove divides and mods from attribute lookups
    #rb shaun.kime, jamie.hayes
    #jira none
    #lockdown nick.whiting
    
    [CL 16008020 by Stu McKenna in ue5-early-access branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x94C30C5C, 0x498548EB, 0x84503C66, 0x0AB7B8FE);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x0CF4E8A3, 0xFE3CBF47, 0xA7C393E2, 0x18CA542C);
