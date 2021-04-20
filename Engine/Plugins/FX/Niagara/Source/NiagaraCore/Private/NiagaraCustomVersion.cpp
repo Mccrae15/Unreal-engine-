1fe42445409e24a50a303bb99ee14ba6ea2fbae4
Stu McKenna <Stu.McKenna@epicgames.com>
2021-04-20 12:32:21 -0400

    - Fix issues around data interfaces not being able to understand the number of attributes they have as the CDO is called into to generate the parameter definition, not the actual data interface
    #rb shaun.kime, jamie.hayes, devon.penney
    #jira UETOP-1270
    #okforgitub public
    #rnx
    
    [CL 16062778 by Stu McKenna in ue5-early-access branch]

--- Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
+++ Engine/Plugins/FX/Niagara/Source/NiagaraCore/Private/NiagaraCustomVersion.cpp
@@ -8,4 +8,4 @@ const FGuid FNiagaraCustomVersion::GUID(0xFCF57AFA, 0x50764283, 0xB9A9E658, 0xFF
 // Register the custom version with core
 FCustomVersionRegistration GRegisterNiagaraCustomVersion(FNiagaraCustomVersion::GUID, FNiagaraCustomVersion::LatestVersion, TEXT("NiagaraVer"));
 
-const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x0CF4E8A3, 0xFE3CBF47, 0xA7C393E2, 0x18CA542C);
+const FGuid FNiagaraCustomVersion::LatestScriptCompileVersion(0x4BF68FEB, 0xF6EC5B4F, 0xB9B14988, 0xC0A8D711);
