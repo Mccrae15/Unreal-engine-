e9423c3bc06715038547bb168d9073b202eef2e7
Matt Peters <Matt.Peters@epicgames.com>
2021-05-11 08:45:50 -0400

    The deprecated UEngine::GetPackageGuid function is not supported when the iodispatcher is enabled, so handle this more gracefully. #jira UE-115301
    #lockdown simon.tourangeau
    #ushell-cherrypick of 16059128 by PJ.Kack
    #rnx
    
    [CL 16267492 by Matt Peters in ue5-early-access branch]

--- Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
+++ Engine/Source/Runtime/Engine/Private/UnrealEngine.cpp
@@ -149,6 +149,7 @@ UnrealEngine.cpp: Implements the UEngine class and helpers.
 #include "Rendering/SkeletalMeshRenderData.h"
 #include "Serialization/LoadTimeTrace.h"
 #include "Async/ParallelFor.h"
+#include "IO/IoDispatcher.h"
 
 #if WITH_EDITOR
 #include "Settings/LevelEditorPlaySettings.h"
@@ -9418,6 +9419,11 @@ void UEngine::AddTextureStreamingSlaveLoc(FVector InLoc, float BoostFactor, bool
 FGuid UEngine::GetPackageGuid(FName PackageName, bool bForPIE)
 {
 	FGuid Result(0,0,0,0);
+	// There is no package guid support when using the I/O dispatcher
+	if (FIoDispatcher::IsInitialized())
+	{
+		return Result;
+	}
 	FPackagePath PackagePath;
 	if (!FPackagePath::TryFromMountedName(PackageName.ToString(), PackagePath))
 	{
