f2477540197b69a951780376a4eb113973418c4b
chris caulfield <Chris.Caulfield@epicgames.com>
2021-06-14 18:43:09 -0400

    Chaos - Joint QuasiPbd franmework (velocity solve step)
    #rb zhnglin.geng,cedric.caillaud,jaco.vandyk
    #jira none
    
    #ROBOMERGE-OWNER: chris.caulfield
    #ROBOMERGE-AUTHOR: chris.caulfield
    #ROBOMERGE-SOURCE: CL 16575649 in //UE4/Release-4.26-Chaos/... via CL 16666127
    #ROBOMERGE-BOT: RELEASE (Release-4.27-Chaos -> Main) (v834-16658389)
    
    [CL 16666132 by chris caulfield in Main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/PBDJointConstraints.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/Chaos/PBDJointConstraints.cpp
@@ -331,6 +331,7 @@ namespace Chaos
 		, bUpdateVelocityInApplyConstraints(false)
 		, PreApplyCallback(nullptr)
 		, PostApplyCallback(nullptr)
+		, SolverType(EConstraintSolverType::GbfPbd)
 	{
 	}
 
@@ -1307,6 +1308,7 @@ namespace Chaos
 		{
 			UE_LOG(LogChaosJoint, VeryVerbose, TEXT("  Pair Iteration %d / %d"), PairIt, NumPairIts);
 
+			// This is the same for all SolverType settings (which makes it wrong for the GbfPbd version)
 			Solver.ApplyConstraints(Dt, Settings, JointSettings);
 
 			if (!Solver.GetIsActive() && bChaos_Joint_EarlyOut_Enabled)
@@ -1389,7 +1391,18 @@ namespace Chaos
 
 		for (int32 PairIt = 0; PairIt < NumPairIts; ++PairIt)
 		{
-			Solver.ApplyProjections(Dt, Settings, JointSettings);
+			switch(SolverType)
+			{
+			case EConstraintSolverType::None:
+				break;
+			case EConstraintSolverType::GbfPbd:
+			case EConstraintSolverType::StandardPbd:
+				Solver.ApplyProjections(Dt, Settings, JointSettings);
+				break;
+			case EConstraintSolverType::QuasiPbd:
+				Solver.ApplyVelocityConstraints(Dt, Settings, JointSettings);
+				break;
+			}
 
 			if (!Solver.GetIsActive() && bChaos_Joint_EarlyOut_Enabled)
 			{
