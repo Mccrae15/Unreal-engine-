9c311fbf126c21bbca5183df1a109c3641af0e6b
cedric caillaud <cedric.caillaud@epicgames.com>
2021-06-22 11:09:13 -0400

    Chaos : Small optimization when adding dirty element to the grid
    #rb jaco.vandyk
    #jira none
    
    #ROBOMERGE-SOURCE: CL 16743407 in //UE4/Release-4.26-Chaos/... via CL 16743430
    #ROBOMERGE-BOT: RELEASE (Release-4.27-Chaos -> Main) (v835-16672529)
    
    [CL 16743440 by cedric caillaud in Main branch]

--- Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/AABBTree.h
+++ Engine/Source/Runtime/Experimental/Chaos/Public/Chaos/AABBTree.h
@@ -600,6 +600,7 @@ public:
 			// Remove this element from the Grid
 			DoForOverlappedCells(DirtyElements[DeleteDirtyParticleIdx].Bounds, DirtyElementGridCellSize, DirtyElementGridCellSizeInv, [&](int32 Hash) {
 				ensure(DeleteDirtyParticleIndexFromGridCell(Hash, DeleteDirtyParticleIdx));
+				return true;
 				});
 		}
 		else
@@ -628,6 +629,7 @@ public:
 				DoForOverlappedCells(DirtyElements.Last().Bounds, DirtyElementGridCellSize, DirtyElementGridCellSizeInv, [&](int32 Hash) {
 					ensure(DeleteDirtyParticleIndexFromGridCell(Hash, LastDirtyElementIndex));
 					ensure(AddNewDirtyParticleIndexToGridCell(Hash, DeleteDirtyParticleIdx));
+					return true;
 					});
 			}
 			else
@@ -652,7 +654,9 @@ public:
 				if (!EnoughSpaceInGridCell(Hash))
 				{
 					bAddToGrid = false;
+					return false; // early exit to avoid going through all the cells
 				}
+				return true;
 				});
 		}
 
@@ -660,6 +664,7 @@ public:
 		{
 			DoForOverlappedCells(NewBounds, DirtyElementGridCellSize, DirtyElementGridCellSizeInv, [&](int32 Hash) {
 				ensure(AddNewDirtyParticleIndexToGridCell(Hash, NewDirtyElement));
+				return true;
 				});
 		}
 		else
@@ -691,6 +696,7 @@ public:
 				// Was not able to add it to the grid , so delete element from grid
 				DoForOverlappedCells(NewBounds, DirtyElementGridCellSize, DirtyElementGridCellSizeInv, [&](int32 Hash) {
 						DeleteDirtyParticleIndexFromGridCell(Hash, DirtyElementIndex);
+						return true;
 					});
 				// Add to overflow
 				int32 NewOverflowIndex = DirtyElementsGridOverflow.Add(DirtyElementIndex);
@@ -1127,6 +1133,7 @@ private:
 					{
 						HashEntryForOverlappedCells.Add(*HashEntry);
 					}
+					return true;
 				};
 
 				if (Query == EAABBQueryType::Overlap)
