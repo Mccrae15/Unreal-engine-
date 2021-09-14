f463a93850cffc4466abff51a8229fd336d85caa
cedric caillaud <cedric.caillaud@epicgames.com>
2021-09-14 18:13:17 -0400

    Make sure geometry collection proxy set particles dirty after creation on the PT to make sure the simData is properly updated on the acceleration structure
    edigrate from 17505956
    
    #rb trivial
    #jira none
    
    [CL 17511174 by cedric caillaud in 4.27-Chaos branch]

--- Engine/Source/Runtime/Experimental/Chaos/Private/PhysicsProxy/GeometryCollectionPhysicsProxy.cpp
+++ Engine/Source/Runtime/Experimental/Chaos/Private/PhysicsProxy/GeometryCollectionPhysicsProxy.cpp
@@ -1007,6 +1007,15 @@ void FGeometryCollectionPhysicsProxy::InitializeBodiesPT(Chaos::FPBDRigidsSolver
 			}
 		}
 
+		// call DirtyParticle to make sure the acceleration structure is up to date with all the changes happening here
+		for (int32 TransformGroupIndex = 0; TransformGroupIndex < NumTransforms; ++TransformGroupIndex)
+		{
+			if (FClusterHandle* Handle = SolverParticleHandles[TransformGroupIndex])
+			{
+				RigidsSolver->GetEvolution()->DirtyParticle(*Handle);
+			}
+		}
+
 	} // end if simulating...
 
 }
