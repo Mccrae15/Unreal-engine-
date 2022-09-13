832318307ca2bbc291f320cd6cbf2e56fe7fd523
Lauren Barnes <lauren.barnes@epicgames.com>
2022-09-13 09:24:01 -0400

    Changes to UE-160593. Added virtual to the destructor for readability.
    #jira UE-160593: Update Theme Saving Location
    #rb Lauren.Barnes
    #preflight 631b8988304480f8f8b8334d
    
    [CL 21981691 by Lauren Barnes in 5.1 branch]

--- Engine/Source/Editor/UnrealEd/Private/Settings/EditorStyleSettingsCustomization.h
+++ Engine/Source/Editor/UnrealEd/Private/Settings/EditorStyleSettingsCustomization.h
@@ -33,6 +33,8 @@ public:
 	/** Makes a new instance of this detail layout class for a specific detail view requesting it */
 	static TSharedRef<IDetailCustomization> MakeInstance();
 
+	virtual ~FEditorStyleSettingsCustomization(); 
+
 	/** IDetailCustomization interface */
 	virtual void CustomizeDetails(IDetailLayoutBuilder& DetailLayout) override;
 	
