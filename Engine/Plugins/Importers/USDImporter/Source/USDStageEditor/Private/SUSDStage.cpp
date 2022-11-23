547e965e44e5e9698124d277c1279fc771680140
daniel coelho <daniel.coelho@xa.epicgames.com>
2022-11-23 13:51:00 -0500

    USD: Fix prim properties panel not updating when selecting actors and components via selection sync.
    #jira UE-162466
    #rb Anousack.Kitisa
    #preflight 637e25924364ef8d146e83e4
    #lockdown jeanmichel.dignard
    
    [CL 23252523 by daniel coelho in 5.1 branch]

--- Engine/Plugins/Importers/USDImporter/Source/USDStageEditor/Private/SUSDStage.cpp
+++ Engine/Plugins/Importers/USDImporter/Source/USDStageEditor/Private/SUSDStage.cpp
@@ -2079,6 +2079,11 @@ void SUsdStage::OnViewportSelectionChanged( UObject* NewSelection )
 	if ( PrimPaths.Num() > 0 )
 	{
 		UsdStageTreeView->SelectPrims( PrimPaths );
+
+		if ( UsdPrimInfoWidget )
+		{
+			UsdPrimInfoWidget->SetPrimPath( GetCurrentStage(), *PrimPaths[ PrimPaths.Num() - 1 ] );
+		}
 	}
 }
 
