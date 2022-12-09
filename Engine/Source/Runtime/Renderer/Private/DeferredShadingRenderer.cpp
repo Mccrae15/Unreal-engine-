7d770ccabea0cb687351fff6c3e9108d1b8d33a1
Jeremy Moore <Jeremy.Moore@epicgames.com>
2022-12-09 11:30:41 -0500

    Recreate SceneTextures uniform buffer if we only create the velocity texture late in the frame for translucency.
    #rb Josie.Yang
    #preflight 63935fb4bb6fefa472e4dc51
    #jira UE-172476
    #lockdown Mihnea.Balta
    
    [CL 23464416 by Jeremy Moore in 5.1 branch]

--- Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
+++ Engine/Source/Runtime/Renderer/Private/DeferredShadingRenderer.cpp
@@ -3640,7 +3640,7 @@ void FDeferredShadingSceneRenderer::Render(FRDGBuilder& GraphBuilder)
 
 		if (bShouldRenderVelocities)
 		{
-			const bool bRecreateSceneTextures = !SceneTextures.Velocity;
+			const bool bRecreateSceneTextures = !HasBeenProduced(SceneTextures.Velocity);
 
 			GraphBuilder.SetCommandListStat(GET_STATID(STAT_CLM_TranslucentVelocity));
 			RenderVelocities(GraphBuilder, SceneTextures, EVelocityPass::Translucent, false);
