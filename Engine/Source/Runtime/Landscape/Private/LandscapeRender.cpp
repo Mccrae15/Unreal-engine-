be12865d2f4a8f50a70227fb3ffb3f2f5e4ac5b5
chris tchou <chris.tchou@epicgames.com>
2022-12-13 15:58:47 -0500

    Fix for rare crashes -- it is not allowed to call IsDefaultTexture() or access other UTexture state from the render thread.
    NOTE: This is a custom change for Release-5.1, other branches are handled by https://p4-swarm.epicgames.net/reviews/23469595
    
    #rb jonathan.bard
    #jira UE-172394
    #preflight 6398da200a6715255079cbb2
    
    [CL 23496691 by chris tchou in 5.1 branch]

--- Engine/Source/Runtime/Landscape/Private/LandscapeRender.cpp
+++ Engine/Source/Runtime/Landscape/Private/LandscapeRender.cpp
@@ -1556,11 +1556,12 @@ void FLandscapeComponentSceneProxy::OnTransformChanged()
 		0
 	);
 
-	if (HeightmapTexture)
+	FTextureResource* HeightmapResource = HeightmapTexture ? HeightmapTexture->GetResource() : nullptr;
+	if (HeightmapResource)
 	{
-		LandscapeParams.HeightmapTexture = HeightmapTexture->TextureReference.TextureReferenceRHI;
+		LandscapeParams.HeightmapTexture = HeightmapResource->TextureRHI.GetReference();
 		LandscapeParams.HeightmapTextureSampler = TStaticSamplerState<SF_Point>::GetRHI();
-		LandscapeParams.NormalmapTexture = HeightmapTexture->TextureReference.TextureReferenceRHI;
+		LandscapeParams.NormalmapTexture = HeightmapResource->TextureRHI.GetReference();
 		LandscapeParams.NormalmapTextureSampler = TStaticSamplerState<SF_Bilinear>::GetRHI();
 	}
 	else
@@ -1571,9 +1572,10 @@ void FLandscapeComponentSceneProxy::OnTransformChanged()
 		LandscapeParams.NormalmapTextureSampler = GBlackTexture->SamplerStateRHI;
 	}
 
-	if (XYOffsetmapTexture)
+	FTextureResource* XYOffsetmapResource = XYOffsetmapTexture ? XYOffsetmapTexture->GetResource() : nullptr;
+	if (XYOffsetmapResource)
 	{
-		LandscapeParams.XYOffsetmapTexture = XYOffsetmapTexture->TextureReference.TextureReferenceRHI;
+		LandscapeParams.XYOffsetmapTexture = XYOffsetmapResource->TextureRHI.GetReference();
 		LandscapeParams.XYOffsetmapTextureSampler = TStaticSamplerState<SF_Point>::GetRHI();
 	}
 	else
@@ -3536,22 +3538,30 @@ void FLandscapeComponentSceneProxy::ChangeComponentScreenSizeToUseSubSections_Re
 bool FLandscapeComponentSceneProxy::HeightfieldHasPendingStreaming() const
 {
 	bool bHeightmapTextureStreaming = false;
-
 	if (HeightmapTexture)
 	{
+		// this is technically a game thread value and not render-thread safe, but it shouldn't ever crash, may just be out of date.
+		// there doesn't appear to be any render thread equivalent, the render thread is ignorant of streaming state.
+		// in general, HeightfieldHasPendingStreaming() should only be used if the code is ok with a slightly out of date value being returned.
 		bHeightmapTextureStreaming |= HeightmapTexture->bHasStreamingUpdatePending;
 #if WITH_EDITOR
-		bHeightmapTextureStreaming |= HeightmapTexture->IsCompiling();
+		if (const FTexture2DResource* HeightmapTextureResource = (const FTexture2DResource*)HeightmapTexture->GetResource())
+		{
+			bHeightmapTextureStreaming |= HeightmapTextureResource->IsProxy();
+		}
 #endif
 	}
 
 	bool bVisibilityTextureStreaming = false;
-
 	if (VisibilityWeightmapTexture)
 	{
+		// again, not render thread safe (see above)
 		bVisibilityTextureStreaming |= VisibilityWeightmapTexture->bHasStreamingUpdatePending;
 #if WITH_EDITOR
-		bVisibilityTextureStreaming |= VisibilityWeightmapTexture->IsCompiling();
+		if (const FTexture2DResource* VisibilityTextureResource = (const FTexture2DResource*)VisibilityWeightmapTexture->GetResource())
+		{
+			bVisibilityTextureStreaming |= VisibilityTextureResource->IsProxy();
+		}
 #endif
 	}
 
