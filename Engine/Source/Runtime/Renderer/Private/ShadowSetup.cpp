c8ec0535875173139551fcdc83aebc8d663cfa7f
wouter dek <wouter.dek@epicgames.com>
2023-01-05 07:55:15 -0500

    Fix self-shadow-only rendering for CSM. The projection stencil was blank due to SubjectMeshCommandBuildRequests getting cleared before calling SetupMeshDrawCommandsForProjectionStenciling, causing all shadows to disappear. (Debugged by Gabor Szaloki)
    #jira UE-172650
    #preflight 63b6a6d9ff7b9ad7034b9f06
    #rb ola.olsson
    #lockdown juan.canada
    
    [CL 23586624 by wouter dek in 5.1 branch]

--- Engine/Source/Runtime/Renderer/Private/ShadowSetup.cpp
+++ Engine/Source/Runtime/Renderer/Private/ShadowSetup.cpp
@@ -2383,8 +2383,8 @@ void FProjectedShadowInfo::GatherDynamicMeshElements(FSceneRenderer& Renderer, F
 		Renderer.MeshCollector.ProcessTasks();
 	}
 
-	SetupMeshDrawCommandsForShadowDepth(Renderer, InstanceCullingManager);
 	SetupMeshDrawCommandsForProjectionStenciling(Renderer, InstanceCullingManager);
+	SetupMeshDrawCommandsForShadowDepth(Renderer, InstanceCullingManager);
 }
 
 void FProjectedShadowInfo::GatherDynamicMeshElementsArray(
